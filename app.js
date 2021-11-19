ecf498a984577ae24fe72da9ae0cfbb85c9129eb
Josh <jdoleary91@gmail.com>
2021-11-18 21:35:05 -0500

    initial commit

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const indexRouter = require("./routes/index");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,87 @@
 const express = require("express");
 const router = express.Router();
 const data = require("../data/sample.json");
+const Fuse = require("fuse.js");
+
+const options = {
+  isCaseSensitive: false,
+  findAllMatches: false,
+  minMatchCharLength: 2,
+	shouldSort: true,
+  keys: [
+    "loan_number",
+    "first_name",
+		"last_name",
+		"city",
+  ]
+};
+const options_first = {
+  isCaseSensitive: false,
+  findAllMatches: false,
+  minMatchCharLength: 2,
+	shouldSort: true,
+  keys: [
+    "first_name",
+  ]
+};
+const options_last = {
+  isCaseSensitive: false,
+  findAllMatches: false,
+  minMatchCharLength: 2,
+	shouldSort: true,
+  keys: [
+		"last_name",
+  ]
+};
+const options_city = {
+  isCaseSensitive: false,
+  findAllMatches: false,
+  minMatchCharLength: 2,
+	shouldSort: true,
+  keys: [
+		"city",
+  ]
+};
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
-	res.render("index", {
-		title: "Express"
-	});
+router.get("/", function(req, res) {
+	res.render("index");
 });
 
-router.get("/data", function(req, res, next) {
-	res.json(data);
+router.get("/data", function(req, res) {
+	res.render('all_loans', { loans: data });
 })
+
+router.get("/number", function(req, res) {
+	const loan_num = req.query.loan_number;
+	const positive_match = data.find(element => element.loan_number === loan_num);
+	positive_match ? res.render('singleLoan', { loan: positive_match }) : res.send('No matching loans') 
+})
+
+router.get("/search", function(req, res) {
+	const pattern = req.query.query;
+	const fuse = new Fuse(data, options);
+	let search_result = fuse.search(pattern)
+	res.render('result', { loans: search_result });
+})
+router.get("/search/first", function(req, res) {
+	const pattern = req.query.first;
+	const fuse = new Fuse(data, options_first);
+	let search_result = fuse.search(pattern)
+	res.render('result', { loans: search_result });
+})
+router.get("/search/last", function(req, res) {
+	const pattern = req.query.last;
+	const fuse = new Fuse(data, options_last);
+	let search_result = fuse.search(pattern)
+	res.render('result', { loans: search_result });
+})
+router.get("/search/city", function(req, res) {
+	const pattern = req.query.city;
+	const fuse = new Fuse(data, options_city);
+	let search_result = fuse.search(pattern)
+	res.render('result', { loans: search_result });
+})
+
 
 module.exports = router;
